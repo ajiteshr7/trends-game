211fdc835d031b7e6cfd19751b0359a009b980d4
Ajitesh Rai <ajiteshr7@gmail.com>
2017-09-02 08:59:31 +0530

    Use cors proxy

--- js/app.js
+++ js/app.js
@@ -3,12 +3,11 @@
     'use strict';
 
     angular.module('gameApp', ['ui.sortable'])
-        .controller('ListController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+        .controller('ListController', ['$scope', '$http', '$window', '$sce', function ($scope, $http, $window, $sce) {
             var data = [];
-            $http({
-                method: 'GET',
-                url: 'https://trends.google.com/trends/hottrends/visualize/internal/data'
-            }).then(function successCallback(response) {
+        
+            $http.get('https://cors-anywhere.herokuapp.com/https://trends.google.com/trends/hottrends/visualize/internal/data')
+            .then(function successCallback(response) {
                 // this callback will be called asynchronously
                 // when the response is available
                 // console.log(response.data.india);
